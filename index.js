6da44339d2c70b2e5c7ab4d828af91ef9172e550
Bunny <apbhasan@gmail.com>
2025-11-13 17:25:08 +0600

    Update all files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config(); //  .env থেকে PORT
+require('dotenv').config(); 
 
 const app = express();
 const port = process.env.PORT || 3000;
